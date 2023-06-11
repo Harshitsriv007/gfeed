8887089ed241bc3b9b6f801bcbf78218a5adcd41
Harshitsriv007 <harshit.srivastv726@gmail.com>
2023-06-12 00:16:11 +0530

    Antd table implmented

---
+++ src/components/pannel/columntable.jsx
@@ -0,0 +1,29 @@
+const columns = [ 
+    { 
+    key: "name", 
+    title: "Name", 
+    dataIndex: "name", 
+    }, 
+    { 
+    key: "email", 
+    title: "Email", 
+    dataIndex: "email", 
+    }, 
+    { 
+    key: "address", 
+    title: "Address", 
+    dataIndex: "address", 
+    }, 
+    { 
+    key: "phone", 
+    title: "Phone Number", 
+    dataIndex: "phone", 
+    }, 
+    { 
+    key: "website", 
+    title: "Website", 
+    dataIndex: "website", 
+    }, 
+    ] 
+
+export default columns;
--- src/components/pannel/product.jsx
+++ src/components/pannel/product.jsx
@@ -1,36 +1,24 @@
-import React from "react";
-import data from "./data.json";
-
-const Product = () =>
-{
-    return <>
-            <h1> this is a dashboard!</h1>
-            <table>
-        <thead>
-          <tr>
-            <th>Image </th>
-            <th> Title </th>
-            <th> Quantity </th>
-            <th> Parent ID </th>
-          </tr>
-        </thead>
-            <tbody>
-            <div>
-                {data.map((item,keys)=>{
-                  return (<div key={keys}>
-                        <tr>
-                            <td><img src={item.details.all_images[0]} alt="images"/></td>
-                            <td>{item.details.title}</td>
-                            <td>{item.variants[0].quantity}</td>
-                            <td>{item.variants[0].id}</td>
-                        </tr>
-                    </div>)  
-                })}
-            </div>
-                </tbody>
-            </table>
-        </>
-}
-
+import React,{useState} from "react"; 
+import data from "./datatable"; 
+import columns from "./columntable"; 
+import { Table } from "antd"; 
+const Product = () => { 
+const [Data, setData] = useState(data); 
+return ( 
+    <> 
+        <div className="app"> 
+            <div className="table"> 
+                <Table 
+                    dataSource={Data} 
+                    columns={columns} 
+                    pagination={{ pageSize: 10, total: 50, showSizeChanger: true }} 
+                /> 
+            </div> 
+        </div> 
+    </> 
+    ); 
+};
 
 export default Product;
+
+
